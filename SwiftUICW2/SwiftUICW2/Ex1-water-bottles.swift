c5c5ba4817830390b222514eb93e3d62172b565c
Adnan Alfadhli <adnan2000611@gmail.com>
2020-12-25 20:30:18 +0300

    Add textfield

--- SwiftUICW2/SwiftUICW2/Ex1-water-bottles.swift
+++ SwiftUICW2/SwiftUICW2/Ex1-water-bottles.swift
@@ -7,6 +7,8 @@
 import SwiftUI
 
 struct Exercise1: View {
+    @State var name = ""
+    @State var water = 1
     var body: some View {
         ZStack {
             Image("charter")
@@ -20,7 +22,10 @@ struct Exercise1: View {
                     .fontWeight(.black)
                 
 //                ADD THE CODE HERE
-                
+                TextField("اكتب اسمك هنا ", text: $name)
+                Stepper("كم بطلآ من الماء تريد ان تتعهد بان تشرب؟", value: $water , in: 1...100)
+                Spacer()
+                Text("اتعهد انا \(name) ان اشرب \(water) من اكواب الماء يوميا والله على ما اقول شهيد").padding().multilineTextAlignment(.center).font(.system(size: 40))
                 
                 Spacer()        
             }.padding()
