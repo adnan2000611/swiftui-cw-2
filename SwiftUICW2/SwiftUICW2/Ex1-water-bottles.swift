fe58eb93670a58954587fae517bf7c23fae46a67
Adnan Alfadhli <adnan2000611@gmail.com>
2020-12-25 20:41:10 +0300

    add fonts and turn off spell check

--- SwiftUICW2/SwiftUICW2/Ex1-water-bottles.swift
+++ SwiftUICW2/SwiftUICW2/Ex1-water-bottles.swift
@@ -22,10 +22,10 @@ struct Exercise1: View {
                     .fontWeight(.black)
                 
 //                ADD THE CODE HERE
-                TextField("اكتب اسمك هنا ", text: $name)
+                TextField("اكتب اسمك هنا ", text: $name).disableAutocorrection(true)
                 Stepper("كم بطلآ من الماء تريد ان تتعهد بان تشرب؟", value: $water , in: 1...100)
                 Spacer()
-                Text("اتعهد انا \(name) ان اشرب \(water) من اكواب الماء يوميا والله على ما اقول شهيد").padding().multilineTextAlignment(.center).font(.system(size: 40))
+                Text("اتعهد انا \(name) ان اشرب \(water) من اكواب الماء يوميا والله على ما اقول شهيد").padding().multilineTextAlignment(.center).font(.custom("SavoyeLetPlain", size: 40))
                 
                 Spacer()        
             }.padding()
